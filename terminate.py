6b2818b47b48c2d1858d5696165189437d61ea25
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2016-02-23 15:31:02 -0600

    adds comments to terminate.py

--- terminate.py
+++ terminate.py
@@ -1,11 +1,16 @@
 import boto3
 
+# terminates newly created instances
+
+# read names of newly created instances from 'instance.txt' file
 with open('instance.txt', 'r') as f:
+    # ec2_create only creates one instance, so the file only has one line
     instance_id = f.readline()
 
 ec2 = boto3.resource('ec2')
 
 instance = ec2.Instance(instance_id)
 
+# terminate the instance and print the response to the console
 response = instance.terminate()
 print(response)
