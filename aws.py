48751f692be27f613e3c88e931bf904df23c05a4
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2016-02-23 15:21:47 -0600

    adds commenting to aws.py

--- aws.py
+++ aws.py
@@ -2,12 +2,19 @@ import boto3
 
 # print out all running ec2 instances and some information about them
 
+# NOTE: for this to work, you must have the proper credentials and configuration
+# in your ~/.aws folder (for UNIX-based systems - you will have to reconfigure
+# it yourself for Microsoft).
+
 # let's use amazon ec2
 ec2 = boto3.resource('ec2')
 
+# show only 'running' instances. Note this will also grab instances that are
+# running, but not passing status checks.
 instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
 )
 
+# print ec2 instance id and type to the console
 for instance in instances:
     print("instance id: {0}, instance type: {1}".format(instance.id, instance.instance_type))
